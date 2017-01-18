748efaae565e9eb14a04dbefae2c044501d0c25b
Michaela-Davis <michaela.delight@gmail.com>
2017-01-17 21:27:47 -0800

    add initial JS variables with comments

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+  // attach a submit listener to our form.
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+
+    // when form is submitted we assign the value from our form input to the variable year
+    var year = parseInt($("input#year").val());
+
+    // call a leapYear() function (which we will write momentarily), pass it the year variable as an argument, and assign the return value (which will be true or false) to the variable result.
+    var result = leapYear(year);
+
+    
+
+    // display the returned boolean value (currently assigned to the variable result) on the web page with the line
+    $("#result").text(result);
+  });
+});
